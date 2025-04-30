4e2222c34a70f71c550e2c20caa7a371d53bfa07
Katari <73192062+Katari-Kityama@users.noreply.github.com>
2025-04-29 19:08:32 -0600

    $comment

---
+++ main.bat
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Used to automate pushing files to the dev/main branch.
+# set working directory if not already in working directory
+cd kyvera.net
+
+git add .
+
+echo "Enter comment:"
+read -r comment
+git commit -m "$comment"
+git push origin main 
+
+@echo off
+REM Used to automate pushing files to the dev/main branch.
+
+REM Change directory to the project folder
+cd kyvera.net 
+
+REM Add all changes to Git
+git add .
+
+REM Ask user for a commit message
+set /p comment=Enter comment: 
+
+REM Commit with the message
+git commit -m "%comment%"
+
+REM Push to main branch
+git push origin main
